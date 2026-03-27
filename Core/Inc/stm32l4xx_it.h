097ea9c0e874b65b7cae6a1419278a1be7673935
gimips <cnick7364@gmail.com>
2026-03-27 16:11:26 +0800

    feat: add SPI3 interrupt handling and update configuration

--- Core/Inc/stm32l4xx_it.h
+++ Core/Inc/stm32l4xx_it.h
@@ -58,6 +58,7 @@ void SysTick_Handler(void);
 void EXTI1_IRQHandler(void);
 void EXTI9_5_IRQHandler(void);
 void EXTI15_10_IRQHandler(void);
+void SPI3_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
